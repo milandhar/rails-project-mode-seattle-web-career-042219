2568d5f1461e4b24a55d40f1b0b70331141e8051
Milan <milan.dhar@gmail.com>
2019-05-28 11:28:35 -0700

    deleted cache files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to @user
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to @user
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :username)
+  end
+
 end
