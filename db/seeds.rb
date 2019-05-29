7108e081c1230a81ee0d0dc545d0b335a2b3f771
Milan <milan.dhar@gmail.com>
2019-05-28 17:39:33 -0700

    added methods to hand class

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,25 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+Round.destroy_all
+Card.destroy_all
+
 testuser = User.create(id: 1, name:"Alex", username: "test", points: 1000)
 testround = Round.create(id: 1, ante_amount: 5, active: true)
+
+
+card1 = Card.create(suit: "hearts", value: "5", is_dealt: true)
+card2 = Card.create(suit: "spades", value: "11", is_dealt: true)
+card3 = Card.create(suit: "diamonds", value: "11", is_dealt: true)
+card4 = Card.create(suit: "clubs", value: "5", is_dealt: true)
+card5 = Card.create(suit: "hearts", value: "10", is_dealt: true)
+
+card6 = Card.create(suit: "spades", value: "8", is_dealt: true)
+card7 = Card.create(suit: "diamonds", value: "9", is_dealt: true)
+card8 = Card.create(suit: "diamonds", value: "10", is_dealt: true)
+card9 = Card.create(suit: "clubs", value: "11", is_dealt: true)
+card10 = Card.create(suit: "hearts", value: "12", is_dealt: true)
+
+test_user_hand = Card.all[1..5]
+test_dealer_hand = Card.all[6..10]
