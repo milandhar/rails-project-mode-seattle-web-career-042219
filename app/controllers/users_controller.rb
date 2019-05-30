35fc892d94d324fdf663a5ce94fd228261e1443b
Milan <milan.dhar@gmail.com>
2019-05-29 18:01:01 -0700

    added authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show,:edit, :update]
   def index
-    @users = User.all
+
+    if logged_in?
+      @users = User.all
+    else
+      redirect_to new_user_path
+    end
   end
 
   def show
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
 
   def new
@@ -18,20 +23,23 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     @user.update(user_params)
     redirect_to @user
   end
 
-  
 
   private
   def user_params
     params.require(:user).permit(:name, :username)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
