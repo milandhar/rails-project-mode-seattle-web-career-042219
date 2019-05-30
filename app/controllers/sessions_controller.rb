4a793a6dfdc45d8bdf0b384e142becd85de14272
Milan <milan.dhar@gmail.com>
2019-05-30 15:32:21 -0700

    added validation styling for update and create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
 
   def login
-
   end
 
   def logout
@@ -17,7 +16,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to @user
     else
-      flash[:message] = "Incorrect Login"
+      flash[:message] = "Incorrect Login!"
       render :login
     end
   end
